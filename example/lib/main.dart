cc4db320d618612813f5003d32a9e53659ce605c
April丶 <569551020@qq.com>
2022-10-18 22:42:28 +0800

    。

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:spider/log.dart';
-import 'package:spider/novel/bqg99/bqg.dart';
-import 'package:spider/novel/ouoou/bean/chapter.dart';
-import 'package:spider/novel/ouoou/ouoou.dart';
 import 'package:spider/platform/bilibili/bilibili.dart';
 
 void main() {
@@ -46,43 +43,43 @@ class _MyHomePageState extends State<MyHomePage> {
           ElevatedButton(
             child: const Text('Ou中文网，小说分类接口'),
             onPressed: () {
-              OuOou.allClassifies().then<void>((value) {
-                for (var element in value) {
-                  Log.print(value: () => element);
-                }
-              });
+              // OuOou.allClassifies().then<void>((value) {
+              //   for (var element in value) {
+              //     Log.print(value: () => element);
+              //   }
+              // });
             },
           ),
           ElevatedButton(
             child: const Text('Ou中文网，《黎明之剑》详情接口'),
             onPressed: () {
-              OuOou.novelDetail('ou_347900').then(
-                (value) => Log.print(value: () => value),
-              );
+              // OuOou.novelDetail('ou_347900').then(
+              //   (value) => Log.print(value: () => value),
+              // );
             },
           ),
           ElevatedButton(
             child: const Text('Ou中文网，《黎明之剑》第一章详情'),
             onPressed: () {
-              OuOou.chapterDetail(
-                novelId: 'ou_347900',
-                bean: const ChapterBean(
-                  id: '30237236',
-                  name: '第一章 穿越成一个视角是什么鬼',
-                ),
-              ).then(
-                (value) => Log.print(value: () => value?.paragraphs),
-              );
+              // OuOou.chapterDetail(
+              //   novelId: 'ou_347900',
+              //   bean: const ChapterBean(
+              //     id: '30237236',
+              //     name: '第一章 穿越成一个视角是什么鬼',
+              //   ),
+              // ).then(
+              //   (value) => Log.print(value: () => value?.paragraphs),
+              // );
             },
           ),
           ElevatedButton(
             child: const Text('Ou站内搜索《黎明之剑》'),
             onPressed: () {
-              OuOou.searchNovel('黎明之剑').then((value) {
-                for (var element in value) {
-                  Log.print(tag: '搜索结果', value: () => element);
-                }
-              });
+              // OuOou.searchNovel('黎明之剑').then((value) {
+              //   for (var element in value) {
+              //     Log.print(tag: '搜索结果', value: () => element);
+              //   }
+              // });
             },
           ),
           ElevatedButton(
@@ -111,42 +108,42 @@ class _MyHomePageState extends State<MyHomePage> {
           ElevatedButton(
             child: const Text('笔趣阁搜索《黎明之剑》'),
             onPressed: () {
-              Bqg99.searchNovel('黎明之剑').then((value) {
-                Log.print(tag: '《黎明之剑》搜索结果');
-                for (var element in value) {
-                  Log.print(value: () => element.toMap());
-                }
-              });
+              // Bqg99.searchNovel('黎明之剑').then((value) {
+              //   Log.print(tag: '《黎明之剑》搜索结果');
+              //   for (var element in value) {
+              //     Log.print(value: () => element.toMap());
+              //   }
+              // });
             },
           ),
           ElevatedButton(
             child: const Text('笔趣阁查询《黎明之剑》详情'),
             onPressed: () {
-              Bqg99.novelDetail('1944105').then((value) {
-                if (value == null) {
-                  Log.print(tag: '获取《黎明之剑》详情失败');
-                  return;
-                }
-                Log.print(tag: '《黎明之剑》详情', value: () => value.toMap());
-              });
+              // Bqg99.novelDetail('1944105').then((value) {
+              //   if (value == null) {
+              //     Log.print(tag: '获取《黎明之剑》详情失败');
+              //     return;
+              //   }
+              //   Log.print(tag: '《黎明之剑》详情', value: () => value.toMap());
+              // });
             },
           ),
           ElevatedButton(
             child: const Text('笔趣阁获取《黎明之剑》章节《完本感言》详情'),
             onPressed: () {
-              Bqg99.chapterDetail(
-                novelId: '1944105',
-                chapterId: '308697942',
-              ).then((value) {
-                if (value == null) {
-                  Log.print(tag: '获取章节详情失败');
-                  return;
-                }
-                Log.print(tag: '章节详情', value: () => value.toMap());
-                for (var element in value.paragraphs) {
-                  Log.print(value: () => element);
-                }
-              });
+              // Bqg99.chapterDetail(
+              //   novelId: '1944105',
+              //   chapterId: '308697942',
+              // ).then((value) {
+              //   if (value == null) {
+              //     Log.print(tag: '获取章节详情失败');
+              //     return;
+              //   }
+              //   Log.print(tag: '章节详情', value: () => value.toMap());
+              //   for (var element in value.paragraphs) {
+              //     Log.print(value: () => element);
+              //   }
+              // });
             },
           ),
         ]),
